b67114a453959ec79092f865adc6323a4a2ce4ea
Cranky Bawler <6666lenia6666@mail.ru>
2023-03-02 09:57:43 +0300

    п. 3

--- src/Employee.java
+++ src/Employee.java
@@ -1,5 +1,20 @@
 public class Employee {
+    static int count = 0;
     String fio;
     int department;
     int salary;
+    int id;
+
+    public Employee(String fio, int department, int salary) {
+        this.fio = fio;
+        this.department = department;
+        this.salary = salary;
+        this.id = count;
+        count++;
+    }
+
+
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee one = new Employee("Иванов иан ", 1, 5000);
+        Employee fa = new Employee("Ивановdas иан ", 1, 5000);
+        Employee the = new Employee("Иванов fdg ", 2, 505000);
+        System.out.println(Employee.count);
+
     }
 }
