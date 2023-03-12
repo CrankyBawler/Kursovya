21ded8e7c9d8a00dfc61e55c3adfc624c648ab48
Cranky Bawler <6666lenia6666@mail.ru>
2023-03-12 13:06:43 +0300

    Basic

--- src/Employee.java
+++ src/Employee.java
@@ -9,97 +9,48 @@ public class Employee {
         this.fio = fio;
         this.department = department;
         this.salary = salary;
-        this.id = idCount;
-        idCount++;
+        this.id = idCount++;
+
     }
 
-    public String getFio() {
-
-        return this.fio;
-    }
-    public int getDepartment() {
-        return this.department;
-    }
-    public int getSalary() {
-        return this.salary;
-    }
     public int getId() {
         return this.id;
     }
-    public void setFio(String fio) {
-        this.fio = fio;
+
+    public String getFio() {
+        return fio;
     }
-    public void setDepartment(int fio) {
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setDepartment(int department) {
         this.department = department;
     }
+
     public void setSalary(int Salary) {
         this.salary = salary;
+
     }
+
+    @Override
     public String toString() {
-        return id + ". " + fio + ". Отдел: " + department + ". Заработная плата: " + salary + " руб.";
+        return "Employee{" +
+                "id=" + id +
+                ", fio='" + fio + '\'' +
+                ", department=" + department +
+                ", salary=" + salary +
+                '}';
     }
-
-      public static Employee[] toPrint(Employee [] a) {
-        for (int i = 0; i < a.length; i++) {
-            System.out.println(a[i]);
-        }
-        return a;
-    }
-    public static Employee[] toPrintFio(Employee [] a) {
-        for (int i = 0; i < a.length; i++) {
-            System.out.println(a[i].getFio());
-        }
-        return a;
-    }
-
-
-
-    public static int sum (Employee[] arr) {
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            int a = arr[i].getSalary();
-            sum = sum + a;
-        }
-        return sum;
-    }
-    public static int toPrintSum (Employee[]arr) {
-        Employee.sum(arr);
-        System.out.println("Сумма затрат на зарплаты в месяц составляет " + Employee.sum(arr) + " рублей.");
-        return Employee.sum(arr);
-    }
-    public static int min (Employee[] arr) {
-        int a = 200_000;
-        String b = null;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].getSalary()<= a){
-                a = arr[i].getSalary();
-                b = arr[i].getFio();
-            }
-        }
-        System.out.println("Минимальная заработная плата составляет " + a + " рублей, у сторудника " + b);
-        return a;
-    }
-        public static int max (Employee[] arr) {
-        int a = 0;
-        String b = null;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].getSalary()>= a) {
-                a = arr[i].getSalary();
-                b = arr[i].getFio();
-            }
-        }
-        System.out.println("Максимальная заработная плата составляет " + a + " рублей, у сторудника " + b);
-        return a;
-    }
-    public static float middle (Employee[] arr) {
-        Employee.sum(arr);
-        int a = arr.length;
-        float middle = (float) Employee.sum(arr) / a;
-        System.out.println("Средняя сумма заработной платы составила " + middle);
-        return middle;
-    }
-
 }
+
+
+
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ import javax.naming.PartialResultException;
 import java.sql.SQLOutput;
 
 public class Main {
+    static Employee[] employees = new Employee[10];
     public static void main(String[] args) {
-        Employee[] employees = new Employee[10];
         employees[0] = new Employee("Бобров Ефим Тимофеевич", 4, 26_136);
         employees[1] = new Employee("Гуляев Тихон Константинович", 4, 56_272);
         employees[2] = new Employee("Шашков Савелий Георгьевич", 1, 29_104);
@@ -17,11 +17,58 @@ public class Main {
         employees[8] = new Employee("Лазарева Хильда Дмитрьевна", 5, 69_719);
         employees[9] = new Employee("Уварова Дана Евгеньевна", 5, 27_679);
 
-        Employee.toPrint(employees);
-        Employee.toPrintSum(employees);
-        Employee.min(employees);
-        Employee.max(employees);
-        Employee.middle(employees);
-        Employee.toPrintFio(employees);
+        printEmployess();
+        System.out.println();
+        System.out.println("Общая сумма зарплат составила " + sum());
+        System.out.println();
+        System.out.println("Минимльная сумма зарплаты " + minSalary());
+        System.out.println();
+        System.out.println("Максимальная сумма зарплаты " + maxSalary());
+        System.out.println();
+        System.out.println("Средняя сумма зарплаты " + averageSalary());
+        System.out.println();
+        printFIO();
+
+    }
+    static void printEmployess() {
+    for (Employee employee : employees) {
+        System.out.println(employee);
+    }
+}
+    static int sum() {
+        int sum = 0;
+        for (Employee employee : employees) {
+            sum += employee.getSalary();
+        }
+        return sum;
+    }
+    static Employee minSalary() {
+        Employee min = null;
+        for (Employee employee : employees) {
+            if (min == null|| employee.getSalary()< min.getSalary()) {
+                min = employee;
+            }
+        }
+        return min;
+    }
+
+    static Employee maxSalary() {
+        Employee max = null;
+        for (Employee employee : employees) {
+            if (max == null || employee.getSalary() > max.getSalary()) {
+                max = employee;
+            }
+        }
+        return max;
+    }
+
+    static double averageSalary() {
+        return (double) sum() / employees.length;
+    }
+
+    static void printFIO() {
+        for (Employee employee : employees) {
+            System.out.println(employee.getFio());
+        }
     }
 }
